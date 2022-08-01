21c9e689504066a76abe8b084573c29c1292a7d3
seyyedali <echali1373@yahoo.com>
2022-08-01 13:00:22 +0430

    test firstScanf

--- calculation.c
+++ calculation.c
@@ -4,6 +4,43 @@
 
 int main()
 {
+    float a;
+    char b;
+    float c;
+
+    printf("please enter your numbers as (+,-,*,/,%%)\n");
+    scanf("%f %c %f",&a,&b,&c);
+
+    switch (b)
+    {
+    case '+':
+        printf("+");
+        break;
+        
+    case '-':
+        printf("-");
+        break;
+        
+    case '*':
+        printf("*");
+        break;
+        
+    case '/':
+        printf("/");
+        break;
+    
+    case '%':
+        printf("%%");
+        break;
+    
+    
+    
+    
+    default:
+        break;
+    }
+
+
 
 
     return 0;
